540fdf35f1e910260a3899c4390a826065cdfc41
Pablo Henrique <pablo.henrique@cubos.io>
2022-01-12 11:09:01 -0300

    feat: :zap: react-query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import GlobalContext from './context';
 import Routes from './routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GlobalContext>
-        <Header title="Cocktails & Drinks" />
+  const queryClient = new QueryClient();
 
-        <div className="flex flex-wrap p-4 space-x-4 -mt-44">
-          <main className="flex w-full justify-center">
-            <div className="w-full bg-yellow-200 p-2 rounded">
-              <Routes />
-            </div>
-          </main>
-        </div>
-      </GlobalContext>
-    </BrowserRouter>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <GlobalContext>
+          <Header title="Cocktails & Drinks" />
+
+          <div className="flex flex-wrap p-4 space-x-4 -mt-44">
+            <main className="flex w-full justify-center">
+              <div className="w-full bg-yellow-200 p-2 rounded">
+                <Routes />
+              </div>
+            </main>
+          </div>
+        </GlobalContext>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
